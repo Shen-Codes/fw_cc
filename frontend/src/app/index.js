2f1d87bbb56bcecb06d9b16bf926b16192fcb4de
Shen-Codes <60067978+Shen-Codes@users.noreply.github.com>
2021-01-21 14:07:57 -0500

    barebones file and folder structure created

---
+++ frontend/src/app/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Form from '../form'
+import Table from '../table'
+
+const App = () => {
+	return (
+		<div>
+			<Form />
+			<Table />
+		</div>
+	)
+}
+
+export default App
+
