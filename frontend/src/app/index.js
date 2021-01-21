76f7805a9a0b4b15216eb936d52927ecbc79d208
Shen-Codes <60067978+Shen-Codes@users.noreply.github.com>
2021-01-21 16:24:10 -0500

    form function completed

--- frontend/src/app/index.js
+++ frontend/src/app/index.js
@@ -1,12 +1,16 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Form from '../form'
 import Table from '../table'
 
+const initialState = [{}];
+
 const App = () => {
+	const [state, setState] = useState(initialState);
+
 	return (
 		<div>
-			<Form />
-			<Table />
+			<Form state={state} setState={setState} />
+			<Table state={state} setState={setState}/>
 		</div>
 	)
 }
--- frontend/src/form/index.js
+++ frontend/src/form/index.js
@@ -1,9 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Form = () => {
+const Form = ({state, setState}) => {
+	const [form, setForm] = useState({
+		description: "",
+		type: "",
+		amount: 0
+	})
+
+	const handleChange = e => {
+		e.preventDefault();
+		const {name, value} = e.target;
+		setForm(prevState => ({
+			...prevState,
+			[name]: value
+		}));
+	}
+	
 	return (
 		<div>
-			
+			<form onsubmit={}>
+				<label>
+					Description:
+					<input 
+						type="text" 
+						value={form.description} 
+						onChange={handleChange}
+						name="description"
+					/>
+				</label>
+				<label>
+					Type:
+					<select value={form.type} onChange={handleChange}>
+						<option value="asset">Asset</option>
+						<option value="liability">Liability</option>
+					</select>
+				</label>
+				<label>
+					Amount:
+					<input 
+						type="text" 
+						value={form.amount} 
+						onChange={handleChange}
+						name="amount"
+					/>
+				</label>
+			</form>
 		</div>
 	)
 }
